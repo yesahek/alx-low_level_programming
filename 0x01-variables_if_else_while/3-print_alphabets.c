5cc25ed265bad9df4dabe09f300d15ab064b26a5
root <root@DESKTOP-NOHJTPF.localdomain>
2021-06-21 03:59:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: this program Prints out the alphabet
+ * Return: returns 0
+ */
+int main(void)
+{
+  char c, d;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (d = 'A'; d <= 'Z'; d++)
+{
+putchar(d);
+}
+putchar(10);
+return (0);
+}
