458cfe2747a536ba0e793980e94fbbd5f9635e85
root <root@DESKTOP-NOHJTPF.localdomain>
2021-06-21 04:18:59 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: It prints all single digit numbers followed by new line
+ * Return: the function returns 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
