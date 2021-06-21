b9770854693cef97265be0b8a89e14f4b391d948
root <root@DESKTOP-NOHJTPF.localdomain>
2021-06-21 03:28:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: this program Prints out the alphabet
+ * Return: returns 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar(0);
+return (0);
+}
