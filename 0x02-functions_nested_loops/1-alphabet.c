d8f57d4995cf38df46877821f8a2512b016fb0da
root <root@DESKTOP-NOHJTPF.localdomain>
2021-06-22 10:54:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * main - check the code for ALX School students.
- *
+ * print_alphabet - check the code for ALX School students.
+ * Description: It will print all alphabet latter in lowercase.
  * Return: Always 0.
  */
 void print_alphabet(void)
